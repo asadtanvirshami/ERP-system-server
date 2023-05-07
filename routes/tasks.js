121685831932dffe58836631801474ad701b005c
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2023-05-07 10:06:19 -0400

    push

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,29 +4,45 @@ const Op = Sequelize.Op;
 const routes = require("express").Router();
 const { Tasks } = require("../models");
 const { Company, Users } = require("../functions/associations/tasksAssociations");
+const { UserTasks } = require("../functions/associations/userTaskAssociations");
 
 
-routes.post("/createTasks", async (req, res) => {
+routes.post("/createTask", async (req, res) => {
   const task_code =  Math.floor(100 + Math.random() * 9000);
+  const {title, description, priority,deadline, startTime, startDate, bonus, userId} =req.body.data
+  try {
+    await Tasks.create({
+      title:title,
+      description:description,
+      priority:priority,
+      deadline:deadline,
+      start_date:startDate,
+      start_time:startTime,
+      end_date:'pending',
+      end_time:'pending',
+      bonus:bonus,
+      code:task_code,
+      status:'pending',
+      active:true,
+      UserId:userId,
+      CompanyId:companyId
+  })
+    res.status(200).send({ status: "success", message: "Task Created!" });
+  } catch (e) {
+    res.status(304).send({ status: "error", message: "Error Occured" });
+    console.log("Error Occured", e);
+  }
+});
+
+routes.post("/AssignTask", async (req, res) => {
    console.log(req.body)
   var promises = (req.body).map((rqBody)=>{
-    return Tasks.create({
-        title:rqBody.title,
-        description:rqBody.title,
-        priority:rqBody.priority,
-        deadline:rqBody.deadline,
-        start_date:rqBody.startDate,
-        start_time:rqBody.startTime,
-        end_date:'pending',
-        end_time:'pending',
-        bonus:rqBody.bonus,
-        code:task_code,
-        status:'pending',
-        active:true,
-        UserId:rqBody.userId
+    return UserTasks.create({
+       UserId:rqBody.id,
+       CompanyId:rqBody.companyId,
+       TaskId:rqBody.taskId
     })
   })
-
   try {
     await Promise.all(promises)
     res.status(200).send({ status: "success", message: "Task Created!" });
